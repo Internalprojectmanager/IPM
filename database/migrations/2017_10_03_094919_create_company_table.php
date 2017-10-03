6b923a6fec9655d1d09077b03fcf30dcca8c7680
Jeffrey Walst <jeffreywalst@gmail.com>
2017-10-03 12:08:41 +0200

    Added migrations for database (Releases, Projects, Company, Features, Requirements and Users)

--- database/migrations/2017_10_03_094919_create_company_table.php
+++ database/migrations/2017_10_03_094919_create_company_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateCompanyTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email',100)->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('company', function (Blueprint $table) {
+            $table->string('id', 50);
+            $table->string('name', 100)->unique();
+            $table->text('description')->nullable();
+            $table->primary('id');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('company');
     }
 }
--- database/migrations/2017_10_03_094954_create_feature_table.php
+++ database/migrations/2017_10_03_094954_create_feature_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateFeatureTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('first_name',50);
-            $table->string('last_name',50);
-            $table->string('email',100)->unique();
-            $table->string('password',100);
-            $table->rememberToken();
+        Schema::create('feature', function (Blueprint $table) {
+            $table->string('id',50);
+            $table->string('release_id',50)->index();
+            $table->string('name',200);
+            $table->text('description');
+            $table->string('status',50);
+            $table->primary('id');
+
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('feature');
     }
 }
