907ea3c4fb692615254ee5acb2e965f950e02dae
Jeffrey Walst <jeffreywalst@gmail.com>
2017-10-03 13:53:17 +0200

    Added Auth views for login, register and logout

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Auth::routes();
+
+
+
+
+//Route::group(['prefix' => ''], function () {
