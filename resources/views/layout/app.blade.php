aa9a0cdb917dcb6844b04bb67a478180797d43d5
Preston Ziessen <pziessen@itsavirus.com>
2017-10-04 11:05:51 +0200

    Bootstrap toegevoegd

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -12,6 +12,10 @@
 
     <!-- Styles -->
     <link href="{{asset('css/custom.css')}}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 </head>
 <body>
@@ -23,5 +27,7 @@
     </div>
 
 <!-- Javascript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </body>
 </html>
