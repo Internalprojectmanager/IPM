74705ab4d6f253434a19e1735aea6799e89d85de
Preston Ziessen <pziessen@itsavirus.com>
2017-10-04 14:42:43 +0200

    landingpage itsavirus logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,14 @@
 @endsection
 
 @section('content')
+    <div>
+        <img src="https://pbs.twimg.com/profile_images/882269648103854081/eLo7HEbf.jpg" width="150" height="auto" />
+    </div>
     <div class="title m-b-md">
-        IPM Itsavirus
+        IPM ITSAVIRUS<hr>
     </div>
     <div class="m-b-md">
-        Login om deze tool te kunnen gebruiken
+        IPM is alleen beschikbaar voor geautoriseerde gebruikers
     </div>
 
 @endsection
