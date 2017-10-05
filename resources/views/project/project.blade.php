1134fb12b3d87a6a324e953b6d74202b11bc14bd
Jeffrey Walst <jwalst@itsavirus.com>
2017-10-05 12:31:21 +0200

    Fixed routing in projectdetails

--- resources/views/project/project.blade.php
+++ resources/views/project/project.blade.php
@@ -9,7 +9,7 @@
     @foreach($projects as $project)
         <p>
             {{$project->name}}
-            <a href="/project/details/{{$project->name}}/{{$project->company_id}}">Details</a>
+            <a href="{{route('projectdetails', ['name' => $project->name, 'company_id' => $project->company_id])}}">Details</a>
             <a href="{{route('editproject', $project->name)}}">Edit</a>
             <a href="{{route('deleteproject', $project->name)}}">Delete</a>
         </p>
