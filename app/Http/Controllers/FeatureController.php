8895e1585a6f8ea0d2cb0b16c82bacbc70c40a71
Jeffrey Walst <jwalst@itsavirus.com>
2017-10-10 14:04:02 +0200

    Added feature form and fixed feature controller

--- app/Http/Controllers/FeatureController.php
+++ app/Http/Controllers/FeatureController.php
@@ -2,15 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Project;
+use App\Release;
 use Illuminate\Http\Request;
 use App\Feature;
 
 class FeatureController extends Controller
 {
-    public function store(Request $request){
+    public function add($company_id, $name, $release_name){
+        $project = Project::where(['name' => $name, 'company_id' => $company_id])->first();
+        $release = Release::where(['project_id' => $project->id, 'name' => $release_name])->first();
+
+
+        return view('features.add_feature', compact('release', 'project'));
+    }
+
+    public function store($company_id, $name, $release_name, Request $request){
         $feature = new Feature();
 
-        $feature->id = $request->id;
+        $feature->id = $request->feature_id;
         $feature->release_id = $request->release_id;
         $feature->name = $request->name;
         $feature->description = $request->description;
