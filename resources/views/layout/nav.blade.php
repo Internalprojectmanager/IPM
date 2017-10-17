4a28e4b9a41212c3c7b0481368a506a33ba37f80
Jeffrey Walst <jwalst@itsavirus.com>
2017-10-17 12:09:48 +0200

    Fixed content overlapping with menu if content is bigger than screen

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,15 +1,29 @@
-<div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-                <a href="{{ url('/home') }}">Dashboard</a>
-                <a href="{{ route('overviewcompany')}}">Company</a>
-                <a href="{{route('overviewproject')}}">Project</a>
-                @else
-
-                    <a href="{{ route('login') }}">Login</a>
-                    <a href="{{ route('register') }}">Register</a>
-                    @endauth
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">IPM-Tool</a>
         </div>
-    @endif
 
+        <ul class="nav navbar-nav navbar-right">
+            @if (Route::has('login'))
+                @auth
+                    <li><a href="{{ url('/home') }}">Dashboard</a></li>
+                    <li><a href="{{ route('overviewcompany')}}">Company</a></li>
+                    <li><a href="{{route('overviewproject')}}">Project</a></li>
+                    @else
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                        <li><a class="nav-link" href="{{ route('register') }}">Register</a></li>
+                        @endauth
+                    @endif
+        </ul>
+    </div>
+    </div>
+</nav>
