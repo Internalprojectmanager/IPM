4f379407afd94e6af9a9e5bf852fc7469f9dae0e
Robin Geerlings <robingeerlings@hotmail.com>
2017-10-19 13:26:26 +0200

    start controller test report

--- app/Http/Controllers/TestReportController.php
+++ app/Http/Controllers/TestReportController.php
@@ -1,1 +1,14 @@
-<!-- ROBIN KAN JIJ CONTROLLER MAKEN? -->
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class TestReportController extends Controller
+{
+
+}
