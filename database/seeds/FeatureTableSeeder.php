ce92e8121c07b8e350d7801c1d3ce2fdb6501cd7
Jeffrey Walst <jwalst@itsavirus.com>
2017-10-24 18:11:38 +0200

    Filled seeder with more dummy data

--- database/seeds/FeatureTableSeeder.php
+++ database/seeds/FeatureTableSeeder.php
@@ -19,6 +19,7 @@ class FeatureTableSeeder extends Seeder
         \DB::table('feature')->insert(array (
             0 => 
             array (
+                'id' => 23,
                 'release_id' => 'WEBSITEST1',
                 'name' => 'Design choises',
                 'description' => 'Choose a new design for the website',
@@ -29,6 +30,7 @@ class FeatureTableSeeder extends Seeder
             ),
             1 => 
             array (
+                'id' => 24,
                 'release_id' => 'WEBSITEST1',
                 'name' => 'Login',
                 'description' => 'Login for users',
@@ -39,6 +41,7 @@ class FeatureTableSeeder extends Seeder
             ),
             2 => 
             array (
+                'id' => 25,
                 'release_id' => 'WEBSITEST1',
                 'name' => 'Homepage',
                 'description' => 'Create a homepage for the website',
@@ -49,13 +52,58 @@ class FeatureTableSeeder extends Seeder
             ),
             3 => 
             array (
+                'id' => 26,
                 'release_id' => 'WEBSITEST1',
-                'name' => 'About us page with pictures of our team',
+                'name' => 'About us',
                 'description' => 'Create a about page for the website',
                 'status' => 'Open',
                 'author' => 'test test',
                 'created_at' => '2017-10-17 11:32:59',
                 'updated_at' => '2017-10-17 11:32:59',
+            ),
+            4 => 
+            array (
+                'id' => 27,
+                'release_id' => 'WEBSITEST1',
+                'name' => 'Contact Page',
+                'description' => NULL,
+                'status' => 'Open',
+                'author' => 'test test',
+                'created_at' => '2017-10-24 15:35:19',
+                'updated_at' => '2017-10-24 15:35:19',
+            ),
+            5 => 
+            array (
+                'id' => 28,
+                'release_id' => 'WEBSITEST1',
+                'name' => 'Work Page',
+                'description' => NULL,
+                'status' => 'Testing',
+                'author' => 'test test',
+                'created_at' => '2017-10-24 15:35:19',
+                'updated_at' => '2017-10-24 15:35:19',
+            ),
+            6 => 
+            array (
+                'id' => 29,
+                'release_id' => 'WEBSITEST1',
+                'name' => 'Updates Page',
+                'description' => NULL,
+                'status' => 'Open',
+                'author' => 'test test',
+                'created_at' => '2017-10-24 16:10:18',
+                'updated_at' => '2017-10-24 16:10:18',
+            ),
+            7 => 
+            array (
+                'id' => 30,
+                'release_id' => 'WEBSITEST1',
+                'name' => 'Approach Page',
+                'description' => NULL,
+                'status' => 'In Progress',
+                'author' => 'test test',
+                'created_at' => '2017-10-24 16:10:18',
+                'updated_at' => '2017-10-24 16:10:18',
             ),
         ));
         
