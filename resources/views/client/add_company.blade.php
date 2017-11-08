5e345841ffba86fddac0ae578bfa1600be02b54c
Jeffrey Walst <jwalst@itsavirus.com>
2017-11-08 11:36:55 +0100

    routes/

--- resources/views/client/add_company.blade.php
+++ resources/views/client/add_company.blade.php
@@ -1,10 +1,10 @@
 @extends('layout.app')
 
 @section('title')
-    Edit company
+    Add client
 @endsection
 
-@section('breadcrumbs', Breadcrumbs::render('editcompany', $companys))
+@section('breadcrumbs', Breadcrumbs::render('addclient'))
 
 @section('content')
 
@@ -17,16 +17,16 @@
             </ul>
         </div>
     @endif
-    <form action="{{route('updatecompany', $companys->name)}}" method="post">
+
+    <form action="{{route('storeclient')}}" method="post">
         {{ csrf_field() }}
         <h3>Company</h3>
         <div class="form-group">
-            <input type="hidden" name="name" value="{{$companys->name}}">
-            <label for="company_name">Company name:</label>
-            <input type="text" class="form-control" name="company_name" id="company_name" value="{{$companys->name}}">
+            <label for="client_name">Company name:</label>
+            <input type="text" class="form-control" name="client_name" id="client_name">
             <br><br>
             <label for="description">Description:</label>
-            <textarea rows="4" cols="50" name="description" class="form-control" id="description">{{$companys->description}}</textarea>
+            <textarea rows="4" cols="50" name="description" class="form-control" id="description"></textarea>
         </div>
         <button class="btn btn-primary" type="submit">Submit</button>
     </form>
