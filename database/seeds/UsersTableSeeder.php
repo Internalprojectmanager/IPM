2581fefff473cadabec8bfb3cb9df5e81f5971ab
Jeffrey Walst <jwalst@itsavirus.com>
2017-11-14 11:40:24 +0100

    Changed userseeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -22,7 +22,7 @@ class UsersTableSeeder extends Seeder
                 'first_name' => 'test',
                 'last_name' => 'test',
                 'email' => 'iav@itsavirus.com',
-                'password' => bcrypt('Avocad0'),
+                'password' => bcrypt('avocad0'),
                 'remember_token' => NULL,
                 'created_at' => '2017-10-12 15:12:37',
                 'updated_at' => '2017-10-12 15:12:37',
