2b389c333ec25dbfd619a3c9ca472596c3eea6d9
Preston Ziessen <pziessen@itsavirus.com>
2017-11-16 11:42:15 +0100

    count projects

--- public/js/custom.js
+++ public/js/custom.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
     $(".search").keyup(function () {
         var searchTerm = $(".search").val();
         var listItem = $('.results tbody').children('tr');
-        var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
+        var searchSplit = searchTerm.replace(/ /g, "'):containsi('");
 
         $.extend($.expr[':'], {'containsi': function(elem, i, match, array){
             return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
@@ -45,6 +45,11 @@ $(document).ready(function() {
 
         var jobCount = $('.results tbody tr[visible="true"]').length;
         $('.counter').text(jobCount + ' Projects');
+
+        if(jobCount == '1') {
+            $('.results tbody tr[visible="true"]').length;
+            $('.counter').text(jobCount + ' Project');
+        }
 
         if(jobCount == '0') {$('.no-result').show();}
         else {$('.no-result').hide();}
