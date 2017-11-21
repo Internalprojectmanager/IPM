197fc6b87c176fada45b46e5a165e88824e5106d
Jeffrey Walst <jwalst@itsavirus.com>
2017-11-21 12:18:16 +0100

    Changed company to client

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,11 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
         $this->call(ProjectTableSeeder::class);
-        $this->call(CompanyTableSeeder::class);
         $this->call(ReleaseTableSeeder::class);
         $this->call(FeatureTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(ClientTableSeeder::class);
     }
 }
