89eae4fd55cc0df73aac71b210b9b73f743c7279
Preston Ziessen <pziessen@itsavirus.com>
2017-11-23 17:43:26 +0100

    Changed status code

--- database/seeds/StatusTableSeeder.php
+++ database/seeds/StatusTableSeeder.php
@@ -4,88 +4,98 @@ use Illuminate\Database\Seeder;
 
 class StatusTableSeeder extends Seeder
 {
+
     /**
-     * Run the database seeds.
+     * Auto generated seed file
      *
      * @return void
      */
     public function run()
     {
+        
+
         \DB::table('status')->delete();
-
+        
         \DB::table('status')->insert(array (
-            0 =>
-                array (
-                    'id' => 1,
-                    'name' => 'Draft',
-                    'type' => 'Progress',
-                    'color' => '#417505'
-                ),
-            1 =>
-                array (
-                    'id' => 2,
-                    'name' => 'In Progress',
-                    'type' => 'Progress',
-                    'color' => '#5D99CA'
-                ),
-            2 =>
-                array (
-                    'id' => 3,
-                    'name' => 'Paused',
-                    'type' => 'Progress',
-                    'color' => '#9B9B9B'
-                ),
-            3 =>
-                array (
-                    'id' => 4,
-                    'name' => 'Cancelled',
-                    'type' => 'Progress',
-                    'color' => '#FF3300'
-                ),
-            4 =>
-                array (
-                    'id' => 5,
-                    'name' => 'Testing',
-                    'type' => 'Progress',
-                    'color' => ''
-                ),
-            5 =>
-                array (
-                    'id' => 6,
-                    'name' => 'Completed',
-                    'type' => 'Progress',
-                    'color' => '#9B9B9B'
-                ),
-
-            6 =>
-                array (
-                    'id' => 7,
-                    'name' => 'Client',
-                    'type' => 'Client',
-                    'color' => '#417505'
-                ),
-
-            7 =>
-                array (
-                    'id' => 8,
-                    'name' => 'Lead',
-                    'type' => 'Client',
-                    'color' => '#5D99CA'
-                ),
-            8 =>
-                array (
-                    'id' => 9,
-                    'name' => 'Prospect',
-                    'type' => 'Client',
-                    'color' => '#FF3300'
-                ),
-            9 =>
-                array (
-                    'id' => 10,
-                    'name' => 'No Partners Anymore',
-                    'type' => 'Client',
-                    'color' => '#9B9B9B'
-                ),
+            0 => 
+            array (
+                'id' => 1,
+                'name' => 'Draft',
+                'type' => 'Progress',
+                'color' => '#5D99CA',
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'name' => 'In Progress',
+                'type' => 'Progress',
+                'color' => '#F5A623',
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'name' => 'Paused',
+                'type' => 'Progress',
+                'color' => '#CECECE',
+            ),
+            3 => 
+            array (
+                'id' => 4,
+                'name' => 'Cancelled',
+                'type' => 'Progress',
+                'color' => '#FF3300',
+            ),
+            4 => 
+            array (
+                'id' => 5,
+                'name' => 'Testing',
+                'type' => 'Progress',
+                'color' => '#BD10E0',
+            ),
+            5 => 
+            array (
+                'id' => 6,
+                'name' => 'Completed',
+                'type' => 'Progress',
+                'color' => '#7ED321',
+            ),
+            6 => 
+            array (
+                'id' => 7,
+                'name' => 'Client',
+                'type' => 'Client',
+                'color' => '#B8E986',
+            ),
+            7 => 
+            array (
+                'id' => 8,
+                'name' => 'Lead',
+                'type' => 'Client',
+                'color' => '#F5A623',
+            ),
+            8 => 
+            array (
+                'id' => 9,
+                'name' => 'Prospect',
+                'type' => 'Client',
+                'color' => '#5D99CA',
+            ),
+            9 => 
+            array (
+                'id' => 10,
+                'name' => 'No Partners Anymore',
+                'type' => 'Client',
+                'color' => '#9B9B9B',
+            ),
+            10 => 
+            array (
+                'id' => 11,
+                'name' => 'Final',
+                'type' => 'Progress',
+                'color' => '#727AA0',
+            ),
         ));
+        
+        
     }
 }
