d3dcddac82da9d3a767a301813b132f3df4aba07
Preston Ziessen <pziessen@itsavirus.com>
2017-11-29 13:24:38 +0100

    pdf template

--- resources/views/release/pdf.blade.php
+++ resources/views/release/pdf.blade.php
@@ -13,7 +13,7 @@
         <p id="p1">
             <img class="logo-p1" src="{{asset('img/logo-iav-circles.png')}}">
             <span class="h1-p1">PROJECT SPECIFICATION</span>
-            <span class="h2-p1">{{$project->name}} RELEASE {{$release->name}} {{$release->version}}</span>
+            <span class="h2-p1">{{$project->name}} {{$release->name}} {{$release->version}}</span>
             <span class="h3-p1"><?php echo date("d - m - Y"); ?></span>
 
             <span class="project-info-p1">
@@ -78,95 +78,136 @@
         <p id="p4">
             <span class="h1">PROJECT<br>DESCRIPTION</span>
             <br><br><br>
-            <span class="h2">PROJECT DESCRIPTION</span><br><br>
+            <span class="h2" style="margin-bottom: 600px;">PROJECT DESCRIPTION</span><br>
             <span class="project-description">
                 {{ $project->description }}
             </span>
             <br><br><br><br>
             <span class="h2">PROJECT ROLES & RESPONSIBILITIES</span><br><br><br>
 
+        <span>
             <table class="table-p4">
-            <thead>
-            <tr class="bold-text-p4" align="center">
-                <th align="center" width="40%">ROLE</th>
-                <th align="left" width="25%">NAMED</th>
-                <th align="center" width="45%">RESPONSIBILITY</th>
-            </tr>
-            </thead>
+                <thead>
+                <tr class="bold-text-p4" align="center">
+                    <th align="center" width="35%">ROLE</th>
+                    <th align="left" width="25%">NAMED</th>
+                    <th align="center" width="50%">RESPONSIBILITY</th>
+                </tr>
+                </thead>
 
-            <tbody>
-            <tr>
-                <th><br></th>
-                <th><br></th>
-                <th><br></th>
-            </tr>
+                <tbody>
+                <tr>
+                    <th><br></th>
+                    <th><br></th>
+                    <th><br></th>
+                </tr>
 
-            <tr>
-                <td align="center" class="bold-text-p4">Project manager(s)</td>
-                <td>
-                    <span class="bold-text-p4">Nick Vogel</span><br>
-                    <span class="company-p4">Itsavirus</span><br><br>
-                    <span class="bold-text-p4">Erik Odijk</span><br>
-                    <span class="company-p4">Itsavirus</span><br>
-                </td>
-                <td class="responsibility-p4">
-                    Responsible for developing a definition of the
-                    project. The Project Manager also ensures that
-                    the project is deliveredon time, to budget and
-                    to the required quality standard (within agreed
-                    specifications).
-                </td>
-            </tr>
+                <tr>
+                    <td align="center" class="bold-text-p4">Project manager(s)</td>
+                    <td>
+                        <span class="bold-text-p4">Jochem Verheul</span><br>
+                        <span class="company-p4">Itsavirus</span><br><br>
+                        <span class="bold-text-p4">Chris Dawe</span><br>
+                        <span class="company-p4">Itsavirus</span><br>
+                    </td>
+                    <td class="responsibility-p4">
+                        Responsible for developing a definition of the
+                        project. The Project Manager also ensures that
+                        the project is deliveredon time, to budget and
+                        to the required quality standard (within agreed
+                        specifications).
+                    </td>
+                </tr>
 
-            <tr>
-                <td><hr></td>
-                <td><hr></td>
-                <td><hr></td>
-            </tr>
+                <tr>
+                    <td><hr></td>
+                    <td><hr></td>
+                    <td><hr></td>
+                </tr>
 
-            <tr>
-                <td align="center" class="bold-text-p4">Designer(s)</td>
-                <td>
-                    <span class="bold-text-p4">Nick Vogel</span><br>
-                    <span class="company-p4">Itsavirus</span><br><br>
-                    <span class="bold-text-p4">Erik Odijk</span><br>
-                    <span class="company-p4">Itsavirus</span><br>
-                </td>
-                <td class="responsibility-p4">
-                    Staff who actively work on the design process
-                    of the project.
-                </td>
-            </tr>
+                <tr>
+                    <td align="center" class="bold-text-p4">Designer(s)</td>
+                    <td>
+                        <span class="bold-text-p4">Nick Vogel</span><br>
+                        <span class="company-p4">Itsavirus</span><br><br>
+                        <span class="bold-text-p4">Erik Odijk</span><br>
+                        <span class="company-p4">Itsavirus</span><br>
+                    </td>
+                    <td class="responsibility-p4">
+                        Staff who actively work on the design process
+                        of the project.
+                    </td>
+                </tr>
 
-            <tr>
-                <td><hr></td>
-                <td><hr></td>
-                <td><hr></td>
-            </tr>
+                <tr>
+                    <td><hr></td>
+                    <td><hr></td>
+                    <td><hr></td>
+                </tr>
 
-            <tr>
-                <td align="center" class="bold-text-p4">Developer(s)</td>
-                <td>
-                    <span class="bold-text-p4">Nick Vogel</span><br>
-                    <span class="company-p4">Itsavirus</span><br><br>
-                    <span class="bold-text-p4">Erik Odijk</span><br>
-                    <span class="company-p4">Itsavirus</span><br>
-                </td>
-                <td class="responsibility-p4">
-                    Staff who actively work on the develop process
-                    of the project.
-                </td>
-            </tr>
+                <tr>
+                    <td align="center" class="bold-text-p4">Developer(s)</td>
+                    <td>
+                        <span class="bold-text-p4">Laurens Verspeek</span><br>
+                        <span class="company-p4">Itsavirus</span><br><br>
+                        <span class="bold-text-p4">Noman Jabbar</span><br>
+                        <span class="company-p4">Itsavirus</span><br>
+                    </td>
+                    <td class="responsibility-p4">
+                        Staff who actively work on the develop process
+                        of the project.
+                    </td>
+                </tr>
 
-            <tr>
-                <td><hr></td>
-                <td><hr></td>
-                <td><hr></td>
-            </tr>
-            </tbody>
-        </table>
+                <tr>
+                    <td><hr></td>
+                    <td><hr></td>
+                    <td><hr></td>
+                </tr>
+                </tbody>
+            </table>
+        </span>
+
         </p>
         <!-- END OF PAGE 4 -->
+
+        <!-- PAGE 5 -->
+        <p>
+            <span class="h2">FEATURE 1.0</span>
+            <span>
+                <table class="table-p5">
+                    <tbody>
+                        <tr class="project-description">
+                            <td width="35%">
+                                Re-Design Website
+                            </td>
+                            <td width="65%">
+                                De homepage wordt een landingspagina waarbij je een introductie krijgt van het Effect Ai project. Hier moet de bezoeker en duidelijke indruk krijgen van het project. In de header zal een duidelijke indruk krijgen van het project. In de header zal een duidelijke slogan en logo getoond worden,
+                                vervolgens zullen er in 6 tot 8 blokken informatie, nieuws, updates, contact worden getoond. Nieuws en update blokken zullen doorverwijzen naar een content pagina.
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </span><br><br><br>
+
+            <span class="h2">FUNCTIONAL REQUIREMENTS</span><br><br>
+            <span>
+                <table class="table-p5">
+                    <tbody>
+                        <tr class="project-description">
+                            <td width="35%">
+                                <strong>FR-1.1</strong>
+                                Header
+                            </td>
+                            <td width="65%">
+                                Hier moet een introtekst komen met een korte uitleg over Effect Ai plus een button om door te klikken onder de header komen 6 tot 8 blokken deze linken naar updates en nieuws. Er kunnen ook blokken tussen zitten met een creatieve interactie.
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </span>
+        </p>
+        <!-- END OF PAGE 5 -->
 
     </main>
 
