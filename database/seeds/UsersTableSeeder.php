3bbc3a6420216f0e83c9b719c902297053e0fc1b
Jeffrey Walst <jwalst@itsavirus.com>
2017-12-12 15:05:13 +0100

    Added dummy data to assingees

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,7 +14,7 @@ class UsersTableSeeder extends Seeder
     {
         
 
-        \DB::table('users')->delete();
+        \DB::table('users')->truncate();
         
         \DB::table('users')->insert(array (
             0 => 
@@ -30,9 +30,9 @@ class UsersTableSeeder extends Seeder
 
             1 =>
                 array (
-                    'first_name' => 'test',
-                    'last_name' => 'test',
-                    'email' => 'iav2@itsavirus.com',
+                    'first_name' => 'Jochem',
+                    'last_name' => 'Verheul',
+                    'email' => 'jverheul@itsavirus.com',
                     'password' => bcrypt('avocad0'),
                     'remember_token' => NULL,
                     'created_at' => '2017-10-12 15:12:37',
@@ -41,9 +41,83 @@ class UsersTableSeeder extends Seeder
 
             2 =>
                 array (
-                    'first_name' => 'test',
-                    'last_name' => 'test',
-                    'email' => 'iav3@itsavirus.com',
+                    'first_name' => 'Laurens',
+                    'last_name' => 'Verspeek',
+                    'email' => 'lverspeek@itsavirus.com',
+                    'password' => bcrypt('avocad0'),
+                    'remember_token' => NULL,
+                    'created_at' => '2017-10-12 15:12:37',
+                    'updated_at' => '2017-10-12 15:12:37',
+                ),
+
+            3 =>
+                array (
+                    'first_name' => 'Nick',
+                    'last_name' => 'Vogel',
+                    'email' => 'nvogel@itsavirus.com',
+                    'password' => bcrypt('avocad0'),
+                    'remember_token' => NULL,
+                    'created_at' => '2017-10-12 15:12:37',
+                    'updated_at' => '2017-10-12 15:12:37',
+                ),
+
+            4 =>
+                array (
+                    'first_name' => 'Jeffrey',
+                    'last_name' => 'Walst',
+                    'email' => 'jwalst@itsavirus.com',
+                    'password' => bcrypt('avocad0'),
+                    'remember_token' => NULL,
+                    'created_at' => '2017-10-12 15:12:37',
+                    'updated_at' => '2017-10-12 15:12:37',
+                ),
+            5 =>
+                array (
+                    'first_name' => 'Preston',
+                    'last_name' => 'Ziessen',
+                    'email' => 'pziessen@itsavirus.com',
+                    'password' => bcrypt('avocad0'),
+                    'remember_token' => NULL,
+                    'created_at' => '2017-10-12 15:12:37',
+                    'updated_at' => '2017-10-12 15:12:37',
+                ),
+            6 =>
+                array (
+                    'first_name' => 'Robin',
+                    'last_name' => 'Geerlings',
+                    'email' => 'rgeerlings@itsavirus.com',
+                    'password' => bcrypt('avocad0'),
+                    'remember_token' => NULL,
+                    'created_at' => '2017-10-12 15:12:37',
+                    'updated_at' => '2017-10-12 15:12:37',
+                ),
+            7 =>
+                array (
+                    'first_name' => 'Mike',
+                    'last_name' => 'van der Berg',
+                    'email' => 'mvanderberg@itsavirus.com',
+                    'password' => bcrypt('avocad0'),
+                    'remember_token' => NULL,
+                    'created_at' => '2017-10-12 15:12:37',
+                    'updated_at' => '2017-10-12 15:12:37',
+                ),
+
+            8 =>
+                array (
+                    'first_name' => 'Chris',
+                    'last_name' => 'Dawe',
+                    'email' => 'cdawe@itsavirus.com',
+                    'password' => bcrypt('avocad0'),
+                    'remember_token' => NULL,
+                    'created_at' => '2017-10-12 15:12:37',
+                    'updated_at' => '2017-10-12 15:12:37',
+                ),
+
+            9 =>
+                array (
+                    'first_name' => 'Noman',
+                    'last_name' => 'Jabbar',
+                    'email' => 'njabbar@itsavirus.com',
                     'password' => bcrypt('avocad0'),
                     'remember_token' => NULL,
                     'created_at' => '2017-10-12 15:12:37',
