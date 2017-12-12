f482b8809d8d94868c26c6913a21b714f60dd400
Jeffrey Walst <jwalst@itsavirus.com>
2017-12-12 14:47:41 +0100

    Fixed Assignee table

---
+++ database/migrations/2017_12_12_131758_update_assigne_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateAssigneTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('assignee', function (Blueprint $table) {
+            $table->dropColumn('project_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('assignee', function (Blueprint $table) {
+            $table->integer('project_id');
+        });
+    }
+}
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -27,6 +27,28 @@ class UsersTableSeeder extends Seeder
                 'created_at' => '2017-10-12 15:12:37',
                 'updated_at' => '2017-10-12 15:12:37',
             ),
+
+            1 =>
+                array (
+                    'first_name' => 'test',
+                    'last_name' => 'test',
+                    'email' => 'iav2@itsavirus.com',
+                    'password' => bcrypt('avocad0'),
+                    'remember_token' => NULL,
+                    'created_at' => '2017-10-12 15:12:37',
+                    'updated_at' => '2017-10-12 15:12:37',
+                ),
+
+            2 =>
+                array (
+                    'first_name' => 'test',
+                    'last_name' => 'test',
+                    'email' => 'iav3@itsavirus.com',
+                    'password' => bcrypt('avocad0'),
+                    'remember_token' => NULL,
+                    'created_at' => '2017-10-12 15:12:37',
+                    'updated_at' => '2017-10-12 15:12:37',
+                ),
         ));
         
         
