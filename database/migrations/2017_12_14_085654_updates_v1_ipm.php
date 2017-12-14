fa245fa9f9b1ad1eed4618b9ca150a2c4a7e13c6
Jeffrey Walst <jwalst@itsavirus.com>
2017-12-14 12:49:29 +0100

    Added some fields to database

---
+++ database/migrations/2017_12_14_085654_updates_v1_ipm.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdatesV1Ipm extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('assignee', function (Blueprint $table) {
+            $table->boolean('status')->nullable()->after('uuid');
+        });
+
+        Schema::table('project', function (Blueprint $table) {
+            $table->string('projectcode', 15)->unique()->after('company_id');
+        });
+
+        Schema::table('release', function (Blueprint $table) {
+            $table->integer('status')->nullable()->after('version');
+            $table->integer('document_status')->nullable()->after('specificationtype');
+            $table->text('extra_content')->nullable()->after('document_status');
+        });
+
+        Schema::table('feature', function (Blueprint $table) {
+            $table->integer('category')->nullable()->after('status');
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('job_title')->nullable()->after('email');
+            $table->boolean('active')->default(0)->after('job_title');
+        });
+
+        Schema::table('client', function (Blueprint $table) {
+            $table->binary('contacts')->nullable()->after('contactemail');
+            $table->binary('link')->nullable()->after('contacts');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
