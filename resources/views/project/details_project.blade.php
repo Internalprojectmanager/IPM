6db762940ef58b5505325f19384a6525300e214b
Robin Geerlings <robingeerlings@hotmail.com>
2017-12-21 14:35:04 +0100

    added edit button to project details

--- resources/views/project/details_project.blade.php
+++ resources/views/project/details_project.blade.php
@@ -8,6 +8,23 @@
 
 @section('content')
 
+    <button class="edit-button" id="project-edit">
+        <svg id="edit-logo" width="21px" height="20px" viewBox="0 0 21 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <!-- Generator: Sketch 47.1 (45422) - http://www.bohemiancoding.com/sketch -->
+            <title>Edit Icon</title>
+            <desc>Created with Sketch.</desc>
+            <defs></defs>
+            <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+                <g id="BUTTON/EDIT" transform="translate(-14.000000, -13.000000)" stroke="#000000">
+                    <g id="edit" transform="translate(15.000000, 13.000000)">
+                        <path d="M19,3.51851852 L15.4814815,0 L1.40740741,14.0740741 L0,19 L4.92592593,17.5925926 L19,3.51851852 Z M12.6666667,2.81481481 L16.1851852,6.33333333 L12.6666667,2.81481481 Z M1.40740741,14.0740741 L4.92592593,17.5925926 L1.40740741,14.0740741 Z" id="Edit-Icon"></path>
+                    </g>
+                </g>
+            </g>
+        </svg>
+        <span class="edit-content" id="edit-content">Edit</span>
+    </button>
+
     <div class="row">
         <div class="header-3" id="project-details">
             <span class="project-title" id="project-name">Project Name</span>
