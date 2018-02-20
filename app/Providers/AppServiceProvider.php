2c94c64f8119fe97ce733691142832a1ab5928b3
Jeffrey Walst <jwalst@itsavirus.com>
2018-02-20 14:13:10 +0100

    Fixed HTTS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        dd(\App::environment());
-        if (env('APP_ENV') === 'production') {
+        if (env('APP_ENV') === 'local' || env('APP_ENV') === 'production') {
             \URL::forceScheme('https');
         }
 
