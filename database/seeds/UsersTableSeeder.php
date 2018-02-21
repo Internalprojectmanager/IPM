43254f94abc384ecd3fc52a54c555ee813ccc4fb
Jeffrey Walst <jwalst@itsavirus.com>
2018-02-21 14:37:32 +0100

    Removed the user delete on seeding

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,9 +13,6 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         
-
-        \DB::table('users')->delete();
-        
         \DB::table('users')->insert(array (
             0 => 
             array (
