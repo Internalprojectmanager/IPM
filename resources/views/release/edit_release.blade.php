315bec15be8c6a7b25ad8d96de16d145c3f55d48
Jeffrey Walst <jwalst@itsavirus.com>
2018-05-23 17:52:21 +0200

    Fixed Release Update (Was a DIV :O)

--- resources/views/release/edit_release.blade.php
+++ resources/views/release/edit_release.blade.php
@@ -15,7 +15,7 @@
 
     <div class="row">
         <div class="header-3 form-group" id="edit-project">
-            <div action="{{route('updaterelease', [$client->path, $project->path, $release->path, $release->version])}}"
+            <form action="{{route('updaterelease', [$client->path, $project->path, $release->path, $release->version])}}"
                  method="post">
                 {{ csrf_field() }}
                     <div class="form-group col-md-6">
@@ -74,8 +74,34 @@
                         </button>
                     </div>
                     <div class="form-group col-md-6" align="right">
-                        <button class="btn btn-primary" type="submit">
-                            Save Project <span class="glyphicon glyphicon-plus"></span>
+                        <button class="save-button" id="save-button" type="submit">
+                            <svg id="save-logo" width="19px" height="19px" viewBox="0 0 19 19" version="1.1"
+                                 xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <!-- Generator: Sketch 48.1 (47250) - http://www.bohemiancoding.com/sketch -->
+                                <title>Save icon</title>
+                                <desc>Created with Sketch.</desc>
+                                <defs></defs>
+                                <g id="Style-Guide" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"
+                                   transform="translate(-1071.000000, -3131.000000)">
+                                    <g id="Group-29" transform="translate(1040.000000, 3100.000000)">
+                                        <g id="Save-icon" transform="translate(31.000000, 31.000000)">
+                                            <path d="M1,0 L1,18 L18,18 L18,0 L19,0 L19,19 L0,19 L0,0 L1,0 Z"
+                                                  id="Combined-Shape" fill="#FFFFFF"></path>
+                                            <rect id="Rectangle-9" fill="#FFFFFF" x="10" y="0" width="9"
+                                                  height="1"></rect>
+                                            <rect id="Rectangle-9" fill="#FFFFFF"
+                                                  transform="translate(9.500000, 4.500000) rotate(-90.000000) translate(-9.500000, -4.500000) "
+                                                  x="5" y="4" width="9" height="1"></rect>
+                                            <path d="M9.5,9.5 L5.96446609,5.96446609" id="Line-2" stroke="#FFFFFF"
+                                                  stroke-linecap="square"></path>
+                                            <path d="M13.5,9.5 L9.96446609,5.96446609" id="Line-2-Copy" stroke="#FFFFFF"
+                                                  stroke-linecap="square"
+                                                  transform="translate(11.500000, 7.500000) scale(-1, 1) translate(-11.500000, -7.500000) "></path>
+                                        </g>
+                                    </g>
+                                </g>
+                            </svg>
+                            <span class="button-content" id="button-save">Save</span>
                         </button>
                     </div>
                 </form>
