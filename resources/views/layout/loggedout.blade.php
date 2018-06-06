20dbdeda616962d338c0e439f90fea57d7099e0a
Jeffrey Walst <jwalst@itsavirus.com>
2018-06-06 17:11:43 +0200

    Fixed Hotjar on seperate App.blade

--- resources/views/layout/loggedout.blade.php
+++ resources/views/layout/loggedout.blade.php
@@ -19,6 +19,20 @@
 
     <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('css/custom.css')}}" rel="stylesheet" type="text/css">
+
+    @if(env('APP_ENV') !== "local")
+        <!-- Hotjar Tracking Code for IPM -->
+            <script>
+                (function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:902819,hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            </script>
+    @endif
 </head>
 <body>
     <nav id="navbar_top">
