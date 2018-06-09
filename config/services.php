b0c9f03afd468319f09d7f276d1fecb79571f855
Jeffrey <jeffreywalst@gmail.com>
2018-06-09 16:39:39 +0000

    Fixed typo

--- config/services.php
+++ config/services.php
@@ -36,7 +36,7 @@ return [
     ],
 
     'google' => [
-        'client_id' => env('GOOLGE_CLIENT_ID'),
+        'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('APP_URL').'/auth/google/callback',
     ],
