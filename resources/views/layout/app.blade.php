38d50ed7339d4b8cc994085711b781c198f86908
Jeffrey Walst <jwalst@itsavirus.com>
2018-07-05 15:58:17 +0200

    Added Favicon to IPM

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -31,7 +31,28 @@
     <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js"
             integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
 
-    @if(env('APP_ENV') !== "local")
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{asset('/img/favicon/apple-touch-icon-57x57.png')}}" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{asset('/img/favicon/apple-touch-icon-114x114.png')}}" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{asset('/img/favicon/apple-touch-icon-72x72.png')}}" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{asset('/img/favicon/apple-touch-icon-144x144.png')}}" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="{{asset('/img/favicon/apple-touch-icon-60x60.png')}}" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="{{asset('/img/favicon/apple-touch-icon-120x120.png')}}" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="{{asset('/img/favicon/apple-touch-icon-76x76.png')}}" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="{{asset('/img/favicon/apple-touch-icon-152x152.png')}}" />
+    <link rel="icon" type="image/png" href="{{asset('/img/favicon/favicon-196x196.png')}}" sizes="196x196" />
+    <link rel="icon" type="image/png" href="{{asset('/img/favicon/favicon-96x96.png')}}" sizes="96x96" />
+    <link rel="icon" type="image/png" href="{{asset('/img/favicon/favicon-32x32.png')}}" sizes="32x32" />
+    <link rel="icon" type="image/png" href="{{asset('/img/favicon/favicon-16x16.png')}}" sizes="16x16" />
+    <link rel="icon" type="image/png" href="{{asset('/img/favicon/favicon-128.png')}}" sizes="128x128" />
+    <meta name="application-name" content="IPM"/>
+    <meta name="msapplication-TileColor" content="#FFFFFF" />
+    <meta name="msapplication-TileImage" content="{{asset('/img/favicon/mstile-144x144.png')}}" />
+    <meta name="msapplication-square70x70logo" content="{{asset('/img/favicon/mstile-70x70.png')}}" />
+    <meta name="msapplication-square150x150logo" content="{{asset('/img/favicon/mstile-150x150.png')}}" />
+    <meta name="msapplication-wide310x150logo" content="{{asset('/img/favicon/mstile-310x150.png')}}" />
+    <meta name="msapplication-square310x310logo" content="{{asset('/img/favicon/mstile-310x310.png')}}" />
+
+@if(env('APP_ENV') !== "local")
         <!-- Hotjar Tracking Code for IPM -->
             <script>
                 (function(h,o,t,j,a,r){
@@ -58,7 +79,7 @@
     @endif
 
 </head>
-<body class="@if(!empty(\Request::route())){{\Request::route()->getName()}}@endif">
+<body class="{{ \Request::route() ? \Request::route()->getName() : "no-route" }} {{ Auth::guest() ? "guest" : "auth" }}">
 
     @include('layout.nav')
 
