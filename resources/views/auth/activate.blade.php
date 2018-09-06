dc63395c97e116083cb5357a4adbfbc263896b76
Jeffrey Walst <jeffreywalst@gmail.com>
2018-09-06 09:41:14 +0200

    Fixed Activate email page

--- resources/views/auth/activate.blade.php
+++ resources/views/auth/activate.blade.php
@@ -1,12 +1,17 @@
 @extends('layout.app')
 
 @section('content')
-    <div class="row">
-        <h1>Please activate your email</h1>
+    <div class="header-3 center" id="edit-project">
+        @if(Auth::user()->active == false)
+            <h1>Welcome to IPM</h1>
+            <h3>Please activate your email address</h3>
+        @else
+            <h1>Please activate your email address</h1>
+        @endif
         @include('partials.single-post-submit', [
-             'name'  =>  '<i class="fas fa-check-circle black fa-2x"></i> Send Activation Mail',
+             'name'  =>  'Send Activation Mail',
              'route' =>  'sendActivationMail',
-             'a_class' => 'btn btn-primary',
+             'a_class' => 'btn btn-save ',
         ])
     </div>
 @endsection
